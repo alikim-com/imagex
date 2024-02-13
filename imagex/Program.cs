8eaa92f5f56faffb11c09b026a5cca73478f60c6
alikim <alikims@gmail.com>
2024-02-13 19:08:08 +1100

    zero dep CRC done

--- imagex/Program.cs
+++ imagex/Program.cs
@@ -3,55 +3,75 @@ using System.Buffers.Binary;
 using System.Diagnostics;
 using System.IO.Hashing;
 
-
-
 namespace imagex;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        //var png = PNG.FromFile(
-        //    "../../../testImages", 
-        //    "redDot_1x1.png");
+        TestCRC32();
+    }
 
+    static void TestCRC32()
+    {
         byte[] data = [
             // 0b00110100, 0b11101100
+            0x89, 0x50, 0x4e, 0x48, 0x0d, 0x63, 0xf8, 0x90,
+            0x54, 0x08, 0xd7, 0x63, 0xf8, 0xcf, 0x00
 
-            //0x89, 0x50, 0x4e, 0x48, 0x0d, 0x63, 0xf8, 0x90,
-            //0x54, 0x08, 0xd7, 0x63, 0xf8, 0xcf, 0x00
-
-            0xff
+            //0xff
             //0x01, 0x02, 0x03, 0x04
             //0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x01, 
             //0x00, 0x00, 0x00, 0x01, 0x08, 0x02, 0x00, 0x00, 0x00
 
         ];
 
-        ulong res = Utils.CRC32(data);
+        Stopwatch watch = new();
+        watch.Start();
 
-        //Utils.Log($"{res:X} ");
-        var strRes = Convert.ToString((long)res, 2).PadLeft(32, '0');
-        var prn = string.Concat(strRes.Select((ch, i) => (i + 1) % 8 == 0 ? ch + " " : ch.ToString()));
-        Utils.Log(prn);
+        ulong res = Utils.CRC(data);
+
+        watch.Stop();
+        Console.WriteLine(watch.Elapsed);
+
+        Utils.PrintLongFmt(res);
 
         Utils.Log("-----");
 
-        var dataNeg = data.Select(b => (byte)~b).ToArray();
-        byte[] checksum = Crc32.Hash(dataNeg);
-        checksum[^1] = (byte)~checksum[^1];
+        // speed test against generic Utils.CRC, which is incompatible
+        // with the version of System.IO.Hashing.Crc32 used in PNG
+        // https://www.w3.org/TR/png/#D-CRCAppendix
 
+        watch.Reset();
+        watch.Start();
+
+        byte[] checksum = Crc32.Hash(data);
+
+        watch.Stop();
+        Console.WriteLine(watch.Elapsed);
 
         string outp = "";
         foreach (byte b in checksum)
-        {
-            //outp += $"{b:X2} ";
             outp += Convert.ToString(b, 2).PadLeft(8, '0') + " ";
-        }
         Utils.Log(outp);
+
+        // Utils.CRC is sligthly slower
+
+        // 00:00:00.0039221
+        // 11010101 11101100 00110110 10001010
+        // -----
+        // 00:00:00.0033360
+        // 00111000 00001001 11111001 10010100
     }
 
-    static void SpeedTest()
+    static void TestLoad()
+    {
+        var png = PNG.FromFile(
+            "../../../testImages",
+            "redDot_1x1.png");
+    }
+
+    static void TestSpeed()
     {
         var beData = Utils.ReadFileBytes("../../../testImages", "redDot_1x1.png");
 
--- imagex/Utils.cs
+++ imagex/Utils.cs
@@ -8,21 +8,24 @@ namespace utils;
 public class Utils
 {
     /// <summary>
-    /// Can compute CRC[1..55], depending on hardcoded polynomial
-    /// uses 64 bit ulong as a container
+    /// Can compute CRC[1..55], depending on hardcoded polynomial;<br/>
+    /// uses 64 bit ulong as a container<br/>
+    /// zero initial CRC, Big Endian data order
     /// </summary>
     /// <param name="dsor">divisor polymonial loaded into highest bits of ulong; <br/>
-    /// currently for CRC32: x32 + x26 + x23 + x22 + x16 + x12 + x11 + x10 + x8 + x7 + x5 + x4 + x2 + x1 + x0
+    /// for CRC32: x32 + x26 + x23 + x22 + x16 + x12 + x11 + x10 + x8 + x7 + x5 + x4 + x2 + x1 + x0
     /// </param>
     /// <param name="dsorLen">actual bit-length of dsor; N + 1 for CRC[N]</param>
     /// <param name="divend">container holding up to 8 bytes of data being processed</param>
     /// <param name="payload">the number of data bits currently in divend, counting from msb</param>
-    static public ulong CRC32(byte[] data)
+    static public ulong CRC(byte[] data)
     {
         ulong divend = 0;
+        // CRC32
         ulong dsor = 0b10000010_01100000_10001110_11011011_10000000_00000000_00000000_00000000;
         int dsorLen = 33;  
 
+        // CRC3
         //ulong dsor = 0b10110000_00000000_00000000_00000000_00000000_00000000_00000000_00000000;
         //int dsorLen = 4;
 
@@ -48,14 +51,14 @@ public class Utils
 
         while (true)
         {
-            PrintDivendBlocks(divend, payload, "div");
+            //PrintDivendBlocks(divend, payload, "div");
 
             int leadZeros = BitOperations.LeadingZeroCount(divend);
 
-            int bytesToload = len - bytesLoaded;
+            int bytesToLoad = len - bytesLoaded;
             bool zeroPayload = leadZeros >= payload;
 
-            if (bytesToload == 0 && zeroPayload) break;
+            if (bytesToLoad == 0 && zeroPayload) break;
 
             if (zeroPayload)
             {
@@ -66,38 +69,36 @@ public class Utils
                 payload -= leadZeros;
                 divend <<= leadZeros;
 
-                PrintDivendBlocks(divend, payload, "zrs");
+                //PrintDivendBlocks(divend, payload, "zrs");
             }
 
-            if (payload < dsorLen)
+            if (payload < dsorLen && bytesToLoad != 0)
             {
                 var availPayloadLen = 64 - payload;
-                var wholeBytes = Math.Min(availPayloadLen / 8, bytesToload);
-                if (wholeBytes > 0)
+                var wholeBytes = Math.Min(availPayloadLen / 8, bytesToLoad);
+                
+                ulong newPayload = 0;
+                for (int i = 0; i < wholeBytes; i++)
                 {
-                    ulong newPayload = 0;
-                    for (int i = 0; i < wholeBytes; i++)
-                    {
-                        newPayload <<= 8;
-                        newPayload |= data[bytesLoaded++];
-                    }
-                    var actPayloadLen = wholeBytes * 8;
-                    newPayload <<= availPayloadLen - actPayloadLen;
-                    divend |= newPayload;
-
-                    payload += actPayloadLen;
-
-                    Log("tal " + Convert.ToString((long)divend, 2).PadLeft(64, '0'));
+                    newPayload <<= 8;
+                    newPayload |= data[bytesLoaded++];
                 }
+                var actPayloadLen = wholeBytes * 8;
+                newPayload <<= availPayloadLen - actPayloadLen;
+                divend |= newPayload;
+
+                payload += actPayloadLen;
+
+                //Log("tal " + Convert.ToString((long)divend, 2).PadLeft(64, '0'));
             }
 
             divend ^= dsor;
 
-            Log("dsr " + Convert.ToString((long)dsor, 2).PadLeft(64, '0'));
+            //Log("dsr " + Convert.ToString((long)dsor, 2).PadLeft(64, '0'));
 
         }
 
-        Log("fin " + Convert.ToString((long)divend, 2).PadLeft(64, '0'));
+        //Log("fin " + Convert.ToString((long)divend, 2).PadLeft(64, '0'));
 
         return divend >> (64 - payload - (dsorLen - 1));
     }
@@ -167,6 +168,13 @@ public class Utils
                 throw new NotImplementedException($"Utils.Log : logMode '{logMode}' is not supported");
         }
 
+    }
+
+    static public void PrintLongFmt(ulong res)
+    {
+        var strRes = Convert.ToString((long)res, 2).PadLeft(32, '0');
+        var prn = string.Concat(strRes.Select((ch, i) => (i + 1) % 8 == 0 ? ch + " " : ch.ToString()));
+        Log(prn);
     }
 
     static public void PrintDivendBlocks(ulong divend, int payload, string pref)
