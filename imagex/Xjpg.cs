326de730eabf01b35d49e6078ba4e011329d9621
alikim <alikims@gmail.com>
2024-05-07 06:37:44 +1000

    huff encoding v2a

--- imagex/Xjpg.cs
+++ imagex/Xjpg.cs
@@ -47,19 +47,62 @@ public partial class SgmSOF0
 
 public partial class SgmDHT
 {
-    static readonly Dictionary<Symb, int> dcSymbFreq = [];
-    static readonly Dictionary<Symb, int> acSymbFreq = [];
+    struct HuffNode
+    {
+        public Symb symb;
+        public int freq;
+        public int[] chIndex;
+    }
 
+    static readonly HuffNode[] _dc = new HuffNode[12];
+    static readonly HuffNode[] _ac = new HuffNode[162];
+
+    // reusable statistics/tree containers for each compId
+
+    static readonly HuffNode[][] compDc = new HuffNode[4][];
+    static readonly HuffNode[][] compAc = new HuffNode[4][];
+
+    /// <summary>
+    /// Create templates
+    /// </summary>
     static SgmDHT()
     {
-        for (byte vbl = 0; vbl < 12; vbl++)
-            dcSymbFreq.Add(new Symb { numZeroes = 0, valBitlen = vbl }, 0);
+        // DC
 
-        acSymbFreq.Add(new Symb { numZeroes = 0, valBitlen = 0 }, 0);
-        acSymbFreq.Add(new Symb { numZeroes = 0xF, valBitlen = 0 }, 0);
+        int cnt = 0;
+        for (byte vbl = 0; vbl < 12; vbl++)
+            _dc[cnt++] = new HuffNode
+            {
+                symb = new Symb { numZeroes = 0, valBitlen = vbl },
+                freq = 0,
+                chIndex = [-1, -1],
+            };
+
+        // AC
+
+        // end of block
+        _ac[0] = new HuffNode
+        {
+            symb = new Symb { numZeroes = 0, valBitlen = 0 },
+            freq = 0,
+            chIndex = [-1, -1],
+        };
+        // 16 zero span
+        _ac[1] = new HuffNode
+        {
+            symb = new Symb { numZeroes = 0xF, valBitlen = 0 },
+            freq = 0,
+            chIndex = [-1, -1],
+        };
+        cnt = 2;
         for (byte nz = 0; nz < 16; nz++)
             for (byte vbl = 1; vbl < 11; vbl++)
-                acSymbFreq.Add(new Symb { numZeroes = nz, valBitlen = vbl }, 0);
+                _ac[cnt++] = new HuffNode
+                {
+                    symb = new Symb { numZeroes = nz, valBitlen = vbl },
+                    freq = 0,
+                    chIndex = [-1, -1],
+                };
     }
 
     public new enum Status
@@ -74,13 +117,15 @@ public partial class SgmDHT
     {
         Status status = Status.None;
 
-        KeyValuePair<ushort, Symb>[][] dcCodesToSymb;
-        KeyValuePair<ushort, Symb>[][] acCodesToSymb;
+        for (int c = 0; c < 4; c++)
+        {
+            compDc[c] = [.. _dc];
+            compAc[c] = [.. _ac];
+        }
 
-        foreach (var kv in dcSymbFreq) acSymbFreq[kv.Key] = 0;
-        foreach (var kv in acSymbFreq) acSymbFreq[kv.Key] = 0;
+        int[] dcDiff = new int[4];
 
-        int[] dcDiff = new int[8];
+        // gather frequency statistics
 
         int cnt = 0;
         foreach (var du in DUnits)
@@ -90,73 +135,97 @@ public partial class SgmDHT
             var compId = du.compId;
             var zigZag = du.zigZag;
 
+            // DC
+
+            var dc = compDc[0];
+
             short dcVal = (short)(zigZag[0] - dcDiff[compId]);
             dcDiff[compId] = dcVal;
             ushort udcVal = (ushort)Math.Abs(dcVal);
 
             byte vbl = (byte)(32 - BitOperations.LeadingZeroCount(udcVal));
-            var symb = new Symb { numZeroes = 0, valBitlen = vbl };
 
-            if (!dcSymbFreq.TryGetValue(symb, out int val))
+            int ind = Array.FindIndex(dc, n => n.symb.numZeroes == 0 && n.symb.valBitlen == vbl);
+            if (ind == -1)
             {
                 status |= Status.SymbolNotInDictionary;
                 return false;
             }
-            dcSymbFreq[symb] = val + 1;
+            dc[ind].freq++;
+
+            // AC
+
+            var ac = compAc[0];
 
             byte nz = 0;
-            var eot = new Symb { };
-            var zblock = new Symb { numZeroes = 0xF, valBitlen = 0};
             for (int i = 1; i < 64; i++)
             {
                 short acVal = zigZag[i];
-                if(acVal == 0)
+                if (acVal == 0)
                 {
                     nz++;
                     if (nz == 16)
                     {
                         nz = 0;
-                        acSymbFreq[zblock]++;
+                        ac[1].freq++; // 16zz
                     }
                     continue;
                 }
                 ushort uacVal = (ushort)Math.Abs(acVal);
 
                 vbl = (byte)(32 - BitOperations.LeadingZeroCount(uacVal));
-                symb = new Symb { numZeroes = nz, valBitlen = vbl };
 
-                nz = 0;
-
-                if (!acSymbFreq.TryGetValue(symb, out val))
+                ind = Array.FindIndex(ac, n => n.symb.numZeroes == nz && n.symb.valBitlen == vbl);
+                if (ind == -1)
                 {
                     status |= Status.SymbolNotInDictionary;
                     return false;
                 }
-                acSymbFreq[symb] = val + 1;
+
+                nz = 0;
+
+                ac[ind].freq++;
             }
-            if(nz != 0) acSymbFreq[eot]++;
+            if (nz != 0) ac[0].freq++; // eob   
         }
+
+
+        // build Huffman tree to find codes' length
+        // tree nodes FF added to the array <- COPY SIZE CHK
+
 
         foreach (var du in DUnits)
             Console.WriteLine(Utils.TableToStr(du.table, 4, 5));
 
-        string dicStr = "DC[0]\n";
-        foreach (var kv in dcSymbFreq)
+        string dicStr = "";
+
+        for (int c = 0; c < 4; c++)
         {
-            var symb = kv.Key;
-            var freq = kv.Value;
-            var numZrs = symb.numZeroes;
-            var valBitlen = symb.valBitlen;
-            dicStr += $"{numZrs:X}/{valBitlen:X} - {freq}\n";
+            var dc = compDc[c];
+
+            dicStr += $"\nDC[{c}]\n";
+            foreach (var node in dc)
+            {
+                var symb = node.symb;
+                var freq = node.freq;
+                var numZrs = symb.numZeroes;
+                var valBitlen = symb.valBitlen;
+                dicStr += $"{numZrs:X}/{valBitlen:X} - {freq}\n";
+            }
         }
-        dicStr += "\nAC[0]\n";
-        foreach (var kv in acSymbFreq)
+
+        for (int c = 0; c < 4; c++)
         {
-            var symb = kv.Key;
-            var freq = kv.Value;
-            var numZrs = symb.numZeroes;
-            var valBitlen = symb.valBitlen;
-            dicStr += $"{numZrs:X}/{valBitlen:X} - {freq}\n";
+            var ac = compAc[c];
+            dicStr += $"\nAC[{c}]\n";
+            foreach (var node in ac)
+            {
+                var symb = node.symb;
+                var freq = node.freq;
+                var numZrs = symb.numZeroes;
+                var valBitlen = symb.valBitlen;
+                dicStr += $"{numZrs:X}/{valBitlen:X} - {freq}\n";
+            }
         }
 
         Console.WriteLine(dicStr);
